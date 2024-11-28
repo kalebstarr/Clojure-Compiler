4dd6047b89bd89d1542a224325ab8513f90468ff
Kaleb Starr <kaleb.starr@proton.me>
2024-11-28 19:29:35 +0100

    Add Instaparse dependency and add simple grammar for C#

--- src/compiler_clojure/core.clj
+++ src/compiler_clojure/core.clj
@@ -1,7 +1,20 @@
 (ns compiler-clojure.core
+  (:require [instaparse.core :as insta])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+(def grammar
+  (insta/parser
+   "S = Namespace
+    Namespace = 'namespace' <Whitespace> Identifier <Whitespace> '{' <Whitespace> Class <Whitespace> '}'
+    Class = 'class' <Whitespace> Identifier <Whitespace> '{' <Whitespace> Method (<Whitespace> Method)* <Whitespace> '}'
+    Method = 'static' <Whitespace> Type <Whitespace> Identifier <Whitespace> '(' <Whitespace> Type <Whitespace> Identifier <Whitespace> ')' <Whitespace> '{' <Whitespace> '}'
+    Type = 'void' | 'int' | 'string'
+    Identifier = #'[a-zA-Z0-9]*'
+    <Whitespace> = #'\\s*'"))
+(grammar "namespace there {
+          class this {
+          static void Main(int args){}
+          static int Main(string args){}}}")
+
+(defn -main [& args]
   (println "Hello, World!"))
