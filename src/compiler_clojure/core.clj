f7803996cbf0aebfae361316e43e61a35889fb9c
Kaleb Starr <kaleb.starr+github@proton.me>
2024-11-30 03:28:20 +0100

    Simplify file reading and clean up grammar
    Replace clojure.java.io/reader with slurp

--- src/compiler_clojure/core.clj
+++ src/compiler_clojure/core.clj
@@ -1,6 +1,5 @@
 (ns compiler-clojure.core
   (:require [instaparse.core :as insta]
-            [clojure.java.io :as io]
             [clojure.string :as str]
             [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
@@ -27,21 +26,28 @@
 
 (defn read-file [filename]
   (try
-    (with-open [rdr (clojure.java.io/reader filename)]
-      (str/join "\n" (line-seq rdr)))
+    (slurp filename)
     (catch java.io.FileNotFoundException e
       (throw (Exception. (str "File was not found: " e))))))
 
 (def csharp-grammar
   (insta/parser
    "S = Program
+    
     Program = Using* Namespace
+
     Using = 'using' Identifier ';'
+    
     Namespace = 'namespace' Identifier '{' Class '}'
+    
     Class = 'class' Identifier '{' Method+ '}'
+    
     Method = 'static' Type Identifier '(' Type Identifier ')' '{' Methodcall+ '}'
-    Methodcall = #'[a-zA-Z0-9.]*' '(' (Methodcall | Identifier) ')' (';' | #'\\s'*)
+    
+    Methodcall = Identifier '(' (Methodcall | Identifier) ')' (';' | #'\\s'*)
+    
     Type = 'void' | 'int' | 'string' | 'string[]'
+    
     Identifier = #'[a-zA-Z0-9.]*'"
    :auto-whitespace :standard))
 
