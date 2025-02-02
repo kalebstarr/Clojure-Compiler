d9aef8505b84aab2ff61f61e9b317461fc492a4f
Kaleb Starr <kaleb.starr+github@proton.me>
2025-02-02 12:31:55 +0100

    Update print and add return for void return methods

--- src/compiler_clojure/code_gen_core.clj
+++ src/compiler_clojure/code_gen_core.clj
@@ -17,18 +17,26 @@
     "void" "V"
 
     "string[]" "[Ljava/lang/String;"
-    
+
     (throw (Exception. (str "Unknown type: " type)))))
 
 (defn generate-method [method]
-  (println (str ".method public static " 
-                (pascal-case->camel-case (last (:method-name (:current-method method))))
-                "("
-                (str/join (map #(convert-type (last (extractor/extract-parameter %))) (:params (:current-method method))))
-                ")"
-                (convert-type (:method-type (:current-method method))))))
+  (str ".method public static "
+       (pascal-case->camel-case (last (:method-name (:current-method method))))
+       "("
+       (str/join (map #(convert-type (last (extractor/extract-parameter %))) (:params (:current-method method))))
+       ")"
+       (convert-type (:method-type (:current-method method)))
+       "\n"
+       (when (= "void" (:method-type (:current-method method)))
+         "return\n")
+       ".end method"))
 
 (defn generate-class [xs]
-  (spit "resources/abc.j" (str ".class public " (last (:class-name xs)) "\n"
-                               ".super java/lang/Object\n\n"
-                               (map generate-method (:class-content xs)))))
+  (println (str ".class public " (last (:class-name xs)) "\n"
+                ".super java/lang/Object\n\n"
+                (str/join "\n\n" (map generate-method (:class-content xs)))))
+;;   (spit "resources/abc.j" (str ".class public " (last (:class-name xs)) "\n"
+;;                                ".super java/lang/Object\n\n"
+;;                                (map generate-method (:class-content xs))))
+  )
